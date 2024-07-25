421c0b27d51e5fb99e09169dc731fb335a287002
sun <ysus05@naver.com>
2024-07-25 17:57:35 +0900

    🐛 Fix: rds db 연결, 설정 변경

--- server/config/db.ts
+++ server/config/db.ts
@@ -4,24 +4,16 @@ import { User } from '../entities/User';
 
 dotenv.config();
 
-const isLocal = process.env.USE_LOCAL_DB === 'true';
-
 export const datasource = new DataSource({
   type: 'postgres',
-  host: isLocal ? process.env.LOCAL_DB_HOST : process.env.TEAM_DB_HOST,
-  port: parseInt(
-    isLocal
-      ? process.env.LOCAL_DB_PORT || '5432'
-      : process.env.TEAM_DB_PORT || '5432',
-    10
-  ),
-  username: isLocal ? process.env.LOCAL_DB_USER : process.env.TEAM_DB_USER,
-  password: isLocal
-    ? process.env.LOCAL_DB_PASSWORD
-    : process.env.TEAM_DB_PASSWORD,
-  database: isLocal ? process.env.LOCAL_DB_NAME : process.env.TEAM_DB_NAME,
-  synchronize: true, // 개발 중에만 사용, 실제 환경에서는 false로 설정 , 엔티티와 스키마 자동 동기화 여부
-  logging: true, // 개발 중에만 사용, 실제 환경에서는 false로 설정, 쿼리 관련 로그 출력 여부
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true, // 개발 중에만 사용, 실제 환경에서는 false로 설정
+  logging: false, // 개발 중에만 사용, 실제 환경에서는 false로 설정
+  ssl: process.env.SSL === 'true' ? { rejectUnauthorized: false } : false,
   entities: [User] // 엔티티 클래스 지정
 });
 
