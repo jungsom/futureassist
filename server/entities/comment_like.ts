ec1950f573d084f6459dcc67bcda3839ea4f7e44
jungsom <junyk0414@naver.com>
2024-08-02 21:39:57 +0900

    엔티티 수정

--- server/entities/comment_like.ts
+++ server/entities/comment_like.ts
@@ -17,17 +17,23 @@ export class Comment_like {
   @PrimaryGeneratedColumn()
   like_id: number;
 
+  @Column()
+  comment_id: number;
+
   @ManyToOne(() => Comment, (comment) => comment.commentLike, {
     onDelete: 'CASCADE'
   })
   @JoinColumn({ name: 'comment_id', referencedColumnName: 'comment_id' })
   comment: Comment;
 
+  @Column()
+  user_id: number;
+
   @ManyToOne(() => User, (user) => user.comment, {
     onDelete: 'CASCADE'
   })
   @JoinColumn({ name: 'user_id', referencedColumnName: 'user_id' })
-  user: number;
+  user: User;
 
   @CreateDateColumn({ name: 'created_at' })
   createdAt?: Date;
