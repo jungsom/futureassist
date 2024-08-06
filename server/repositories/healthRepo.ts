9499212f87bd1187e5283748e7ef1ceee63fd00d
jungsom <junyk0414@naver.com>
2024-08-06 11:05:37 +0900

    Feat: 건강정보 기간 조회로 수정

--- server/repositories/healthRepo.ts
+++ server/repositories/healthRepo.ts
@@ -13,17 +13,30 @@ export const createHealthRecord = async (record: HealthRecords) => {
 };
 
 /** user_id로 전체 건강정보 조회 */
-export const selectHealthRecord = async (userId: number) => {
+export const selectHealthRecord = async (
+  userId: number,
+  from?: Date,
+  to?: Date
+) => {
   try {
-    const result = await datasource.query(
-      `SELECT health_id, height, weight, bloodsugar, cholesterol, bloodpressure,      
+    let query = `
+      SELECT health_id, height, weight, bloodsugar, cholesterol, bloodpressure,      
       TO_CHAR(created_at, 'YYYY-MM-DD') as created_at,
       TO_CHAR(updated_at, 'YYYY-MM-DD') as updated_at
       FROM "health_records"
-      WHERE user_id = $1
-      ORDER BY created_at DESC`,
-      [userId]
-    );
+      WHERE user_id = $1`;
+
+    if (from && to) {
+      query += ` AND created_at >= $2 AND created_at <= $3`;
+    }
+
+    query += ` ORDER BY created_at DESC`;
+
+    const result =
+      from && to
+        ? await datasource.query(query, [userId, from, to])
+        : await datasource.query(query, [userId]);
+
     return result;
   } catch (err) {
     throw err;
